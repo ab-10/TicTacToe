1925a2223e666c1f6cef5a84033b5a9cfd38dc74
Jean-Paul Sartre <armins.bagrats@gmail.com>
2017-04-22 21:32:16 +0530

    Fully implement player tests

--- tests/test-player.py
+++ tests/test-player.py
@@ -1,5 +1,6 @@
 """ Tests the player class
 """
+
 import unittest
 from context import player
 from context import board
@@ -36,37 +37,65 @@ class easyEvalTest(unittest.TestCase):
 class mediumEvalTest(unittest.TestCase):
     """ Tests eval_board() method for player in medium difficulty
     """
+
+    def setUp(self):
+        testPlayer = player.player("X", False, "medium")
+        testBoard = board.board()
+
     # Selects the central square if it's free
     def testCenter(self):
-        # to be implemented
+        testBoard.position = ["0", "O", "2", "X", "4", "5", "6", "7", "8"]
+        self.assertTrue(testPlayer.eval_board(testBoard) == "4")
 
     # Selects the winning square
     def testWinning(self):
-        # to be implemented
+        testBoard.position = ["X", "O", "O", "X", "4", "5", "6", "7", "8"]
+        self.assertTrue(testPlayer.eval_board(testBoard) == "6")
 
     # Allows for a winning move when oponent has central and oposite squares selected
-    def testBlock(self):
-        # to be implemented
+    def testHardBlock(self):
+        testBoard.position = ["0", "1", "O", "3", "O", "5", "X", "7", "8"]
+        self.assertTrue((testPlayer.eval_board(testBoard) == "6") or (testPlayer.eval_board(testBoard) == "8"))
 
-    # Selects the only square
+    # Blocks simple winning move
+    def testEasyBlock(self):
+        testBoard.position = ["O", "X", "X", "O", "4", "5", "6", "7", "8"]
+        self.assertTrue(testPlayer.eval_board(testBoard) == "6")
+
+    # Selects the only move
     def testOnly(self):
-        # to be implemented
+        testBoard.position = ["X", "O", "X", "X", "4", "O", "O", "X", "O"]
+        self.assertTrue(testPlayer.eval_board(testBoard) == "4")
 
 class hardEvalTest(unittest.TestCase):
     """ Tests eval_board() method for player in hard difficulty
     """
+
+    def setUp(self):
+        testPlayer = player.player("X", False, "hard")
+        testBoard = board.board()
+
     # Selects the central square if it's free
     def testCenter(self):
-        # to be implemented
+        testBoard.position = ["0", "O", "2", "X", "4", "5", "6", "7", "8"]
+        self.assertTrue(testPlayer.eval_board(testBoard) == "4")
 
     # Selects the winning square
     def testWinning(self):
-        # to be implemented
+        testBoard.position = ["X", "O", "O", "X", "4", "5", "6", "7", "8"]
+        self.assertTrue(testPlayer.eval_board(testBoard) == "6")
 
-    # Blocks the oponent when central and oposite squares are selected
-    def testBlock(self):
-        # to be implemented
+    # Blocks a winning move when oponent has central and oposite squares selected
+    def testHardBlock(self):
+        testBoard.position = ["0", "1", "O", "3", "O", "5", "X", "7", "8"]
+        self.assertTrue(testPlayer.eval_board(testBoard) == "0")
 
-    # Selects the only square
+    # Blocks simple winning move
+    def testEasyBlock(self):
+        testBoard.position = ["O", "X", "X", "O", "4", "5", "6", "7", "8"]
+        self.assertTrue((testPlayer.eval_board(testBoard) == "6") or (testPlayer.eval_board(testBoard) == "8"))
+
+    # Selects the only move
     def testOnly(self):
-        # to be implemented
+        testBoard.position = ["X", "O", "X", "X", "4", "O", "O", "X", "O"]
+        self.assertTrue(testPlayer.eval_board(testBoard) == "4")

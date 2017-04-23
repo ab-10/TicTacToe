a6a0a0c47e9b165120e7db0789da80ddcdfb6b80
Jean-Paul Sartre <armins.bagrats@gmail.com>
2017-04-23 19:41:04 +0530

    Add move()

--- board.py
+++ board.py
@@ -1,3 +1,9 @@
 class board:
     def __init__(self):
         self.position = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+
+    def move(self, player, spot):
+        """ Records move of a player to board
+        """
+
+        self.position[int(spot)] = player.marker
